1df33a1f91a571076d99a5d14c42aa46fe8a7023
DrittsekK28 <neftaligarrido79@aragon.unam.mx>
2023-12-08 01:01:16 -0600

    Sexto Commit

--- Calendario Gegoriano/Calendario Gegoriano.cpp
+++ Calendario Gegoriano/Calendario Gegoriano.cpp
@@ -9,6 +9,8 @@ void main()
 	MA = 1;
 	AS = A + 1;
     if (M == 1, 3, 5, 7, 8, 10 && D == 31) { printf("%2i %2i %4i", DA, MS, A); }
+	else if (M == 2 && D == 28 && A % 400 == 0) { printf("%2i %2i %4i", DS, M, A); }
+	else if (M == 2 && D == 28 && A % 4 == 0 && A % 100) { printf("%2i %2i %4i", DS, M, A); }
 	else if (M == 2 && D == 28) { printf("% 2i % 2i % 4i", DA, MS, A); }
 	else if (D == 4 && M == 10 && A == 1582) { DS = D + 11; printf("%2i %2i %4i", DS, M, A); }
 	else if (M == 4 && D == 30) { printf("%2i %2i %4i", DA, MS, A); }
