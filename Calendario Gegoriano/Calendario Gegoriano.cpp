101415f3e5b0f0ba45b546572aded67536230449
DrittsekK28 <neftaligarrido79@aragon.unam.mx>
2023-12-07 23:42:43 -0600

    Tercer Commit

--- Calendario Gegoriano/Calendario Gegoriano.cpp
+++ Calendario Gegoriano/Calendario Gegoriano.cpp
@@ -1,8 +1,13 @@
 #include <stdio.h>
 void main()
 {
-	int D, M, A, DS;
+	int D, M, A, DS, MS, AS, DA, MA;
 	scanf_s("%2i", &D); scanf_s("%2i", &M); scanf_s("%4i", &A);
 	DS = D + 1;
-	printf("%2i %2i %4i", DS, M, A);
+	DA = 1;
+	MS = M + 1;
+	MA = 1;
+	AS = A + 1;
+	if (M == 1, 3, 5, 7, 8, 9 && D==31) { printf("%2i %2i %4i", DA, MS, A); }
+	else if (1==1){printf("%2i %2i %4i", DS, M, A);}
 }
